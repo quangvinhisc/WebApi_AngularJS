6035343377069ca1755a23ac2de742956316fdaa
quangvinh93hcm <vinh.dq@felix.vn>
2016-06-15 11:28:12 +0700

    add model entity for domain

---
+++ VinhShop.Model/Models/MenuGroup.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VinhShop.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
