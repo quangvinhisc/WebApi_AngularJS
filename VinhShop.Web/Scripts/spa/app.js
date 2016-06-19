5c4225d858f44c71c9053558d475c219b46ee313
quangvinh93hcm <vinh.dq@felix.vn>
2016-06-19 21:28:27 +0700

    example about controller and view in AngularJS

---
+++ VinhShop.Web/Scripts/spa/app.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+var App = angular.module("myModule", []);
+App.controller("myController", myController);
+myController.$inject = ["$scope"];
+
+function myController($scope) {
+    $scope.message = "Example 1";
+}
