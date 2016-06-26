eac7a4525e079c694b74e3abe623dd3bd4b157d1
quangvinh93hcm <vinh.dq@felix.vn>
2016-06-26 14:27:28 +0700

    filter AngularJS

---
+++ VinhShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="E:\MyProject\VinhShop.Web\Assets/admin/libs/angular/angular.js" />
+(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true) {
+                return 'Kích hoạt';
+            } else {
+                return 'Khóa';
+            }
+        }
+    })
+})(angular.module('vinhshop.common'));
