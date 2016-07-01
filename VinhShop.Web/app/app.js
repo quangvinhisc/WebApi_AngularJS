c7b00f018a34f782aab127cb1e4081dd4b47f8a8
quangvinh93hcm <vinh.dq@felix.vn>
2016-07-02 00:05:02 +0700

    save

--- VinhShop.Web/app/app.js
+++ VinhShop.Web/app/app.js
@@ -5,14 +5,15 @@
          'vinhshop.common'])
         .config(config);
 
-    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
 
-    function config($stateProvider, $urlRouterProvider) {
+    function config($stateProvider, $urlRouterProvider, $locationProvider) {
         $stateProvider.state('home', {
             url: "/admin",
             templateUrl: "/app/components/home/homeView.html",
             controller: "homeController"
         });
         $urlRouterProvider.otherwise('/admin');
+        $locationProvider.html5Mode(true);
     }
 })();
